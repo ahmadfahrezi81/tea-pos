ef397590e64a2e45650b6b5f1c4cac2238db717a
ahmadfahrezi <ahmadfahrezi8127@gmail.com>
2025-09-26 19:18:31 +0700

    ✨ feat(analytics): integrate Vercel Analytics into the layout and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
     title: "POS System",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
