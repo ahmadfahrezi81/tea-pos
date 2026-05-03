4c32cf3512b520b1d72c68ba12f197e9cb1b6d00
ahmadfahrezi <ahmadfahrezi8127@gmail.com>
2026-05-03 17:47:04 +0700

    added empty to silence error

--- apps/seller/next.config.ts
+++ apps/seller/next.config.ts
@@ -64,6 +64,7 @@ const withPWA = withPWAInit({
 
 const nextConfig: NextConfig = {
     reactCompiler: true,
+    turbopack: {},
     allowedDevOrigins: ["busked-florentina-ducally.ngrok-free.dev"],
     env: {
         NEXT_PUBLIC_APP_VERSION: version,
